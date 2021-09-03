85b4220d3f7c398344ae5f3901191fa03b4abbce
CandyAlla <15230497577@163.com>
2021-09-03 10:29:34 +0800

    添加 touchkit 和xlua

---
+++ Assets/Scenes/TestCameraZoom/TestCameraZoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraZoom : MonoBehaviour
+{
+    public Camera _cam;
+    void Start()
+    {
+        var recognizer = new TKPinchRecognizer();
+        recognizer.gestureRecognizedEvent += ( r ) =>
+        {
+            Zoom(r);
+        };
+        TouchKit.addGestureRecognizer( recognizer );
+    }
+
+    private void Zoom(TKPinchRecognizer r)
+    {
+        Debug.Log(r.deltaScale);
+        
+        // _cam.fieldOfView += r.deltaScale;
+    }
+}
